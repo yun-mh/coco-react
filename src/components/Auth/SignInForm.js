4ff2987dcf095b0221f2b3148e35a4d258a69cf2
Minho Yun <minhoyun.dev@gmail.com>
2021-01-18 13:42:30 +0900

    Update

--- src/components/Auth/SignInForm.js
+++ src/components/Auth/SignInForm.js
@@ -22,6 +22,7 @@ const SignInForm = ({ action, setAction }) => {
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
       errors.email = "有効ではないメールアドレスです。";
     }
+    // console.log(errors)
     return errors;
   };
 
